f12f7fc4d8cf9d02df719de5c92e183db13d7495
Kamalesh <kamalesh.ec22@bitsathy.ac.in>
2026-01-06 15:58:52 +0530

    Disable datasource and JPA auto config for Render deployment

--- hcl_project/src/main/java/com/example/hcl_project/HclProjectApplication.java
+++ hcl_project/src/main/java/com/example/hcl_project/HclProjectApplication.java
@@ -2,11 +2,17 @@ package com.example.hcl_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {
+				DataSourceAutoConfiguration.class,
+				HibernateJpaAutoConfiguration.class
+		}
+)
 public class HclProjectApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(HclProjectApplication.class, args);
 	}
